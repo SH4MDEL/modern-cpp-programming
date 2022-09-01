c33e9b7f6668ad07c2815037da23f3de84897307
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-09-01 11:12:12 +0900

    using hash
    본인이 만든 클래스를 unordered_set 또는 map에 삽입하고 싶다면 새로 해시 함수를 정의해 주어야 한다.
    여기서는 int와 string으로 이루어진 클래스의 새로운 해시 함수를 정의하며 그냥 int의 값과, string의 해시 함수를 XOR하여 새로운 해시값을 만들어 주었다.
    템플릿 특수화를 통해 만든 hash<Todo> 함수(Functor)는 std:: 안에 정의되어 있으므로 명시적으로 네임스페이스 안에 정의해 주었다.

--- CppProgramming/CppProgramming/10 - 2. 6. using hash.cpp
+++ CppProgramming/CppProgramming/10 - 2. 6. using hash.cpp
@@ -3,6 +3,24 @@
 #include <unordered_set>
 #include <unordered_map>
 
+template <typename K>
+void print_unordered_set(const std::unordered_set<K>& s) {
+	for (const auto& elm : s) {
+		std::cout << elm << std::endl;
+	}
+}
+
+template <typename K>
+void is_exist(std::unordered_set<K>& s, K key) {
+	auto itr = s.find(key);
+	if (itr != s.end()) {
+		std::cout << key << " 가 존재" << std::endl;
+	}
+	else {
+		std::cout << key << " 가 없다" << std::endl;
+	}
+}
+
 class Todo {
 	int priority;
 	std::string job_desc;
@@ -10,5 +28,39 @@ class Todo {
 public:
 	Todo(int priority, std::string job_desc)
 		: priority(priority), job_desc(job_desc) {}
+
+	bool operator==(const Todo& t) const {
+		if (priority == t.priority && job_desc == t.job_desc) return true;
+		return false;
+	}
+
+	friend std::ostream& operator<<(std::ostream& o, const Todo& t);
+	friend struct std::hash<Todo>;
 };
 
+namespace std {
+	template<>
+	struct hash<Todo> {
+		size_t operator()(const Todo& t) const {
+			hash<string> hash_func;
+			return t.priority ^ (hash_func(t.job_desc));
+		}
+	};
+}
+std::ostream& operator<<(std::ostream& o, const Todo& t) {
+	o << "[중요도 : " << t.priority << "] " << t.job_desc;
+	return o;
+}
+
+int main()
+{
+	std::unordered_set<Todo> todos;
+
+	todos.insert(Todo(1, "농구 하기"));
+	todos.insert(Todo(2, "수학 숙제 하기"));
+	todos.insert(Todo(1, "프로그래밍 프로젝트"));
+	todos.insert(Todo(3, "친구 만나기"));
+	todos.insert(Todo(2, "영화 보기"));
+	todos.insert(Todo(5, "일퀘 하기"));
+	print_unordered_set(todos);
+}
